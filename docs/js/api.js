41a4bb3fa7fa005f043ec94d63f88f92976780d2
JuventinoIbarra <al07133783@tecmilenio.mx>
2026-01-31 02:40:53 -0600

    A

--- docs/js/api.js
+++ docs/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://pintura-naranja-backend.onrender.com/api";
+const API_URL = "https://pintura-naranja-fullstack.onrender.com/api";
 
 async function apiFetch(endpoint, options={}) {
     const token = localStorage.getItem("token");
